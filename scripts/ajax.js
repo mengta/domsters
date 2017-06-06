307a867bf6440e37d3418230da519a8ab58e0577
tianmeng <1655970872@qq.com>
2017-06-06 21:09:54 +0800

    Initial commit

---
+++ scripts/ajax.js
@@ -0,0 +1,16 @@
+/**
+ * Created by tianmeng on 2017/6/5.
+ */
+function regen(){
+    prepareSlideshow();
+    prepareInternalnav();
+
+    preparePlaceholder();
+    prepareGallery();
+
+    stripeTables();
+    highlightRows();
+    displayAbbreviations();
+    focusLabels();
+    prepareForms();
+}
